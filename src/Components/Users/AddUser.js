151fd7fe48a30054f0c336698dd751519edca560
Khurshida-Meem <meemrumi59@gmail.com>
2022-04-05 10:47:22 +0600

    added validation on form

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 
 const AddUser = () => {
 
+    const [uname, setUname] = useState('');
+    const [uage, setUage] = useState('');
+
     const handleSubmit = e =>{
         e.preventDefault();
+        if(uname.trim().length === 0 || uage.trim().length ===0 ) {
+            return;
+        }
+        if(+uage < 1) {
+            return;
+        }
+        setUname('');
+        setUage('');
+    }
+
+    const uNameHandler = e => {
+        setUname(e.target.value)
+    }
+    const uAgeHandler = e => {
+        setUage(e.target.value)
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" value={uname} onChange={uNameHandler} />
                 <label htmlFor="age">Age (Years)</label>
-                <input type="number" id="age" />
+                <input type="number" id="age" value={uage} onChange={uAgeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
